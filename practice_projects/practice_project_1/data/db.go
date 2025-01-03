0ccc3178bc49e867eaa663aef69f2ed3b82efca9
anishsharma21 <anisharma2103@gmail.com>
2025-01-03 17:56:15 +1100

    practice projects - on my own a little more now, trying to figure out a structure that works for me, need a script for creating and destroying the db, need to call it from the main function, using a Makefile too for automations.

---
+++ practice_projects/practice_project_1/data/db.go
@@ -0,0 +1,39 @@
+package data
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// FIXME should be called from main.go in main package based on env
+func DbUp() {
+	db, err := sql.Open("sqlite3", "posts.db")
+	if err != nil {
+		log.Fatalf("Error: could not open/create posts.db: %v\n", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS posts (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	content TEXT NOT NULL
+	);`)
+	if err != nil {
+		log.Fatalf("Error: could not create table: %v\n", err)
+	}
+}
+
+func DbDown() {
+	db, err := sql.Open("sqlite3", "posts.db")
+	if err != nil {
+		log.Fatalf("Error: could not open/create posts.db: %v\n", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`DROP TABLE IF EXISTS posts;`)
+	if err != nil {
+		log.Fatalf("Error: could not drop table: %v\n", err)
+	}
+}
--- practice_projects/practice_project_1/go.mod
+++ practice_projects/practice_project_1/go.mod
@@ -1,3 +1,5 @@
 module example/practice_project_1
 
 go 1.23.4
+
+require github.com/mattn/go-sqlite3 v1.14.24
