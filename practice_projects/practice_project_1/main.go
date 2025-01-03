a932663780c0af660c62e0bacf69cbb7744ac552
anishsharma21 <anisharma2103@gmail.com>
2025-01-03 15:25:13 +1100

    practice projects - wildcards, static dir, file server, handler strategies.

--- practice_projects/practice_project_1/main.go
+++ practice_projects/practice_project_1/main.go
@@ -14,9 +14,29 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("public"))))
+	mux.Handle("GET /posts", GetPosts())
+	mux.Handle("GET /posts/{id}", GetPostById())
+
+	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("public"))))
+	mux.Handle("GET /", http.FileServer(http.Dir(".")))
 
 	fmt.Printf("Server starting on port %s...\n", port)
 	http.ListenAndServe(":"+port, mux)
+}
+
+func GetPosts() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Get posts\n")
+	})
+}
+
+func GetPostById() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		if id == "" {
+			http.Error(w, "id not given", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "Post ID: %s\n", id)
+	})
 }
