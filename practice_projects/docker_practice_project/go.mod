bcf75d3d96e734b89d283774bdafe68347a0e2e5
anishsharma21 <anisharma2103@gmail.com>
2025-01-18 21:40:25 +1100

    practice projects - docker project - initial setup of the sqlite db with the mattn db driver.

--- practice_projects/docker_practice_project/go.mod
+++ practice_projects/docker_practice_project/go.mod
@@ -1,3 +1,5 @@
 module github.com/anishsharma21/Go-web-dev/docker_practice_project
 
 go 1.23.4
+
+require github.com/mattn/go-sqlite3 v1.14.24
--- practice_projects/docker_practice_project/main.go
+++ practice_projects/docker_practice_project/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	_, err := DBInit()
+	if err != nil {
+		log.Fatalf("Could not connect to db: %v\n", err)
+	}
+}
+
+func DBInit() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "app.db")
+	if err != nil {
+		return nil, fmt.Errorf("could not open the database: %v\n", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("could not ping the database: %v\n", err)
+	}
+
+	return db, nil
 }
