f7a6f421e0ff956452820e24da1edf8223d15708
anishsharma21 <anisharma2103@gmail.com>
2025-01-31 22:39:13 +1100

    practice projects - docker managed db and cloud hosting - docker compose file created to setup a local persistent volume for my postgres database with the backend also connected in the same container, learnt about docker compose commands, more commands for docker too, connected to the postgres database with a new type of db driver and connection string method, pretty neat.

--- practice_projects/postgres_newdeploy_project/go.mod
+++ practice_projects/postgres_newdeploy_project/go.mod
@@ -1,3 +1,5 @@
 module github.com/anishsharma21/Go-web-dev/practice_projects/postgres_newdeploy_project
 
 go 1.23.4
+
+require github.com/lib/pq v1.10.9
--- practice_projects/postgres_newdeploy_project/main.go
+++ practice_projects/postgres_newdeploy_project/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	_, err := SetupDb()
+	if err != nil {
+		log.Fatalf("Failed to initialise connection to the database: %v\n", err)
+	}
+	log.Printf("Initialised the database connection succesfully!\n")
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +39,36 @@ func main() {
 		log.Fatalf("HTTP server closed early: %v", err)
 	}
 	log.Println("Server shutdown.")
+}
+
+func SetupDb() (*sql.DB, error) {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to connect to database: %v\n", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to ping the database: %v\n", err)
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS questions (
+	id SERIAL PRIMARY KEY,
+	question TEXT NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);`)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create the 'questions' table: %v\n", err)
+	}
+
+	return db, nil
 }
